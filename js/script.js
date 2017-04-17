4f3ce4a0928777ae5dd5d127a8d350f149c74e2a
Mike Mitchell <mmitchell@weber.edu>
2017-04-17 10:32:47 -0600

    Getting the random display working

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ var quotes = [
 // event listener to respond to "Gimme a quote" button clicks
 
 // when user clicks anywhere on the button, the "printQuote" function is called
-//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 // create a getRandomQuote function that selects a quote object from the quotes array and returns it
 function getRandomQuote () {
@@ -66,3 +66,12 @@ function getRandomQuote () {
 
 // create a printQuote function that calls getRandomQuote and stores it in an object variable
 
+function printQuote () {
+	var pickedQuote = getRandomQuote();
+	var quote = '<p class="quote">' + pickedQuote.quote + '</p>';
+	var name = '<p class="source">' + pickedQuote.name + '</p>';
+	var citation = '<span class="citation">' + pickedQuote.citation + '</span>';
+	var year = '<span class="year">' + pickedQuote.year + '</span>';
+	return document.getElementById('quote-box').innerHTML = quote + name + citation + year;
+
+}
